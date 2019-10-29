28b01f6da2f3a8d7fcf84f36a616b0058f62bf73
Sergio Ramos Santonja <sergioramos55@outlook.es>
2019-10-29 10:45:45 +0100

    Terminado

--- app/src/main/java/com/example/spinners/MainActivity.java
+++ app/src/main/java/com/example/spinners/MainActivity.java
@@ -20,19 +20,30 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         setContentView(R.layout.activity_main);
 
 
-        ArrayAdapter<String> adaptador = new ArrayAdapter<String>(MainActivity.this, R.layout.support_simple_spinner_dropdown_item, semana);
+        /*ArrayAdapter<String> adaptador = new ArrayAdapter<String>(MainActivity.this, R.layout.support_simple_spinner_dropdown_item, semana);
 
         Spinner opciones = (Spinner)findViewById(R.id.Opciones);
         opciones.setAdapter(adaptador);
 
-        opciones.setOnItemSelectedListener(this);
+        opciones.setOnItemSelectedListener(this);*/
 
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.colores, R.layout.support_simple_spinner_dropdown_item);
+
+        Spinner colores = (Spinner)findViewById(R.id.Opciones);
+        colores.setAdapter(adapter);
+
+        colores.setOnItemSelectedListener(this);
     }
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View v, int position,
                                long id) {
-        Toast x = Toast.makeText(this, "El dia elegido es: "+ semana[position], Toast.LENGTH_SHORT);
+        /*Toast x = Toast.makeText(this, "El dia elegido es: "+ semana[position], Toast.LENGTH_SHORT);
+        x.show();*/
+
+        String eleccion = parent.getSelectedItem().toString();
+
+        Toast x = Toast.makeText(this, "El color elegido es: "+ eleccion, Toast.LENGTH_SHORT);
         x.show();
 
     }
